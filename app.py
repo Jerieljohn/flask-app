496079b628c13b3e8e2b0070e5f5c8e17d234d13
sonuzonu <zonu47@gmail.com>
2024-07-12 16:46:37 +0530

    sonu

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/webt')
+@app.route('/webtt')
 def webt():
     data = get_data()
     return render_template('webt.html',data=data)
--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_data():
     mydb = mysql.connector.connect(host = "localhost", user="root",password="astroboy", database = "testdb")
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM students")
+    mycursor.execute("SELECT * FROM studentss")
     result = mycursor.fetchmany()
     mydb.close()
     return result
